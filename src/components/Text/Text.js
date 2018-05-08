2752e0ee8b6fcfe5fdef6b05382d7f323ac63737
ARChilton <adam@dsan.co.uk>
2018-05-08 12:44:21 -0400

    minor update

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -2,48 +2,48 @@ import styled from 'react-emotion'
 
 export const H1 = styled('h1') `
 font-family:${props => props.fontFamily || props.theme.font.fontFamily || 'roboto'};
-font-size:3em;
+font-size:3rem;
 font-weight:${props => props.fontWeight || '400'};
 color:${props => props.color || props.theme.color.h1 || 'inherit'};
 text-align:${props => props.textAlign || 'inherit'};
 @media(max-width:768px){
-  font-size:2.5em;
+  font-size:1.8rem;
   margin-bottom:20px;
 }
 
 `
 export const H2 = styled('h2') `
 font-family:${props => props.fontFamily || props.theme.font.fontFamily || 'roboto'};
-font-size:2em;
+font-size:2rem;
 font-weight:${props => props.fontWeight || '400'};
 color:${props => props.color || props.theme.color.h2 || 'inherit'};
 text-align:${props => props.textAlign || 'inherit'};
 @media(max-width:768px){
-  font-size:1.6em;
+  font-size:1.5rem;
 }
 `
 
 export const H3 = styled('h3') `
 font-family:${props => props.fontFamily || props.theme.font.fontFamily || 'roboto'};
-font-size: 1.5em;
+font-size: 1.5rem;
 margin:4px 0px;
 font-weight:${props => props.fontWeight || '400'};
 color:${props => props.color || props.theme.color.h3 || 'inherit'};
 text-align:${props => props.textAlign || 'inherit'};
 @media(max-width:768px){
-  font-size:1.3em;
+  font-size:1.3rem;
 }
 `
 
 export const H4 = styled('h4') `
 font-family:${props => props.fontFamily || props.theme.font.fontFamily || 'roboto'};
-font-size: 1.2em;
+font-size: 1.2rem;
 margin:4px 0px;
 font-weight:${props => props.fontWeight || '400'};
 color:${props => props.color || props.theme.color.h4 || 'inherit'};
 text-align:${props => props.textAlign || 'inherit'};
 @media(max-width:768px){
-  font-size:1.1em;
+  font-size:1.1rem;
 }
 `
 
